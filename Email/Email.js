b6367be076b9713156ab1e10dc4406cebf765624
viveksahu06 <viveksahu1006@gmail.com>
2025-03-05 11:36:46 +0530

    [Vivek] Add. Ensure @ and validate the mandatory 2nd part i.e. bridgelabz

--- Email/Email.js
+++ Email/Email.js
@@ -1,13 +1,14 @@
 function isValidEmail(email) {
-    const emailRegex = /^abc([a-zA-Z0-9._%+-]*)@bridgelabz\.co(\.[a-zA-Z]{2,})?$/;
+    const emailRegex = /^abc([a-zA-Z0-9._%+-]*)@bridgelabz\.[a-zA-Z]{2,}$/;
     return emailRegex.test(email);
 }
 
 console.log(isValidEmail("abc@bridgelabz.co")); // true
 console.log(isValidEmail("abc.xyz@bridgelabz.co.in")); // true
-console.log(isValidEmail("abc123@bridgelabz.co")); // true
-console.log(isValidEmail("abc-xyz@bridgelabz.co.us")); // true
+console.log(isValidEmail("abc-123@bridgelabz.com")); // true
 console.log(isValidEmail("xyz@bridgelabz.co.in")); // false
 console.log(isValidEmail("abc@xyz.co.in")); // false
 console.log(isValidEmail("abc@bridgelabzcom")); // false
 console.log(isValidEmail("abc@bridgelabz.")); // false
+console.log(isValidEmail("abc@bridgelabz.co.a")); // false
+console.log(isValidEmail("abc@randomlabz.co")); // false
