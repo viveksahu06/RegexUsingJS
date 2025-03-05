79674ccae2dcdd1a186575f3495353f96143f1a9
viveksahu06 <viveksahu1006@gmail.com>
2025-03-05 12:08:37 +0530

    [Vivek] Add .Finally lets close the expression with supporting optional parts.

--- Email/Email.js
+++ Email/Email.js
@@ -1,5 +1,5 @@
 function isValidEmail(email) {
-    const emailRegex = /^abc([._,+-][a-zA-Z0-9]+)?@bridgelabz\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/;
+    const emailRegex = /^abc([._,+-][a-zA-Z0-9]+)?@bridgelabz\.[a-zA-Z]{2,}(\.[a-zA-Z]{2})?$/;
     return emailRegex.test(email);
 }
 
@@ -9,3 +9,10 @@ console.log(isValidEmail("user@bridgelabz.in")); // true
 console.log(isValidEmail("xyz@bridgelabz.co.in")); // true
 console.log(isValidEmail("abc@xyz.co.in")); // false
 console.log(isValidEmail("user@randomlabz.com")); // false
+console.log(isValidEmail("abc-xyz@bridgelabz.com")); // true
+console.log(isValidEmail("abc_xyz@bridgelabz.in")); // true
+console.log(isValidEmail("abc+xyz@bridgelabz.com.au")); // true
+console.log(isValidEmail("abc,xyz@bridgelabz.co.uk")); // true
+console.log(isValidEmail("abc@bridgelabz.com")); // true
+console.log(isValidEmail("abc@bridgelabz.co.a")); // false
+console.log(isValidEmail("abc@bridgelabz.")); // false
